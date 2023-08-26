401324e742b1c7cff75d5056cf42d26e47c22773
Viktor Ristic <vktr.r2@protonmail.com>
2023-08-26 11:09:58 -0400

    fix React-easy issue

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -2,13 +2,21 @@
 import "./index.css";
 
 function App() {
-    return (
-        <>
-            <div className="center">
-                {/* TODO: Implement the card component */}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="center">
+        <h1>React/Vite - Easy</h1>
+        <div className="card">
+          <img
+            src="https://loremflickr.com/320/240"
+            alt="Randomly generated cat photo"
+          />
+          <h2>Card Title</h2>
+          <p>Card Description</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
